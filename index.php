56a09f5b58d1594e8ec401bf453cf2cca7e0dab0
Kevin Williams <kwilliams@priworks.com>
2016-01-18 09:45:29 -0800

    setup theme build system

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  Templates.protostar
+ *
+ * @copyright   Copyright (C) 2016 Kevin O. Williams, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+
+<head>
+	<jdoc:include type="head" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+</head>
+
+<body>
+	<jdoc:include type="modules" name="top" /> 
+	<jdoc:include type="component" />
+	<jdoc:include type="modules" name="bottom" />
+</body>
+
+</html>
